37342411b8ddd429a25b28164fbdb67b54e4864c
patfrib52 <frichot.p@gmail.com>
2023-02-06 21:53:06 +1100

    create a function to display produce the div

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const container = document.querySelector('#container');
-const br = document.createElement('br');
-const div2 = document.createElement('div');
+container = document.querySelector('#container');
+
+
 
 function createBoxs(row) {
     for(let j = 1; j <= row; j ++){
         for (let i = 1 ; i <= row; i++){
             const div = document.createElement('div');
-            div.classList.add('boxs')
-            div2.classList.add('space')
-            div.textContent = [i];
+            div.classList.add('boxs');
+            // div.textContent = [i]; 
             container.appendChild(div); 
+            div.addEventListener('mouseover', () => {
+                div.classList.add('color');
+            });
         }  
-}
     }
-    
+}
+
 createBoxs(16);
 
 
